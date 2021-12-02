1c2bc38645e690b02c070fe3f0e455edd168c430
jefferson <jefferson.nascimento@fastsolucoes.com.br>
2021-12-01 22:35:06 -0300

    Adicionando valores temporarios ao perfil

--- Front-end/BancoCrud/src/app/Tela2/Perfil/Perfil.component.ts
+++ Front-end/BancoCrud/src/app/Tela2/Perfil/Perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DadosConta } from 'src/app/models/DadosConta';
+import { RotasService } from 'src/app/services/rotas.service';
 
 @Component({
   selector: 'app-Perfil',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+  profileName = this.dadosConta.nome;
+  saldoAccount = this.dadosConta.saldo;
 
-  constructor() { }
+  constructor(private rotasService: RotasService,
+    private dadosConta:DadosConta) { }
 
   ngOnInit() {
   }
+
 
 }
